d31a3d61b99196f147eacc6c8bef7f1c8bb4b752
GMiraa <guilherme.mira@sptech.school>
2025-05-05 20:17:16 -0300

    Criação parte de login e cadastro do site

---
+++ src/models/usuarioModel.js
@@ -0,0 +1,28 @@
+var database = require("../database/config")
+
+function autenticar(Email, Senha) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", Email, Senha)
+    var instrucaoSql = `
+        SELECT idUsuario, Nome, Email FROM usuario WHERE Email = '${Email}' AND Senha = '${Senha}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+// Coloque os mesmos parâmetros utilizados na Controller. Vá para a var instrucaoSql
+function cadastrar(Nome, Email, Senha, TipoJogador) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", Nome, Email, Senha, TipoJogador);
+    
+    // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
+    //  e na ordem de inserção dos dados.
+    var instrucaoSql = `
+        INSERT INTO usuario (Nome, Email, Senha, TipoJogador) VALUES ('${Nome}', '${Email}', '${Senha}', '${TipoJogador}');
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+module.exports = {
+    autenticar,
+    cadastrar
+};
