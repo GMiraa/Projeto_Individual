c95ae6cdf310882a1fb359322be51f035e6d2bd4
GMiraa <guilherme.mira@sptech.school>
2025-05-29 15:43:49 -0300

    criação da tela de perfil juntamente com seu respectivo CSS

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -6,7 +6,7 @@ function validarSessao() {
     var b_usuario = document.getElementById("b_usuario");
     
     if (email != null && nome != null) {
-        b_usuario.innerHTML = `Olá, ${nome}`;
+        b_usuario.innerHTML = `Olá,<a id="perfil" href="./perfil.html"> ${nome}</a>`;
     } else {
         window.location = "./login.html";
     }
